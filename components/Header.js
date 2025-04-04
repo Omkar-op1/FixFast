2ea7b8b56587f158733c1f080a6e20b8e8deb91a
vj999431@gmail.com <Vj999431@gmail.com>
2025-04-04 14:59:39 +0530

    hi

--- components/Header.js
+++ components/Header.js
@@ -32,7 +32,7 @@ export default function Header() {
     <header className="bg-white shadow-sm">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold text-blue-600">
-          FixFast
+         FixFast
         </Link>
 
         {/* Desktop Navigation */}
